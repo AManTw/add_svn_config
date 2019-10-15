3c0163008c6d87c63f3e9477554c19fae2048255
AMan <jerr83553@gmail.com>
2019-10-15 11:09:10 +0800

    1. add notice about bash and dash - Bad substitution 2. change the count $() and ${}

--- add_svn_code.sh
+++ add_svn_code.sh
@@ -1,5 +1,8 @@
 # !/bin/bash
 
+# [NOTICE]: It must be used in "Bash"
+# Ubuntu is "dash"``, it will cause Bad substitution
+# when cutting string to $res
 
 do_main(){
 
@@ -9,11 +12,12 @@ do_main(){
 
 	STR=`grep extensionAllowedProposedApi /tmp/product.json`
 	line=`grep -n extensionAllowedProposedApi /tmp/product.json| awk -F':' '{print $1}' `
-	count=`echo $STR |wc -c `
-	res=`echo "   "${STR:0:$count-2}", \"johnstoncode.svn-scm\"],"`
-	sed -i  ''"$line"'s/^.*$/'"$res"'/' /tmp/product.json
+	count=$(echo $STR |wc -c)
+	res=`echo "   "${STR:0:$((count-2))}", \"johnstoncode.svn-scm\"],"`
 	
-   sudo cp /tmp/product.json /usr/share/code/resources/app/product.json
+	sed -i ''"$line"'s/^.*$/'"$res"'/' /tmp/product.json
+	
+    sudo cp /tmp/product.json /usr/share/code/resources/app/product.json
 
 }
 do_main
